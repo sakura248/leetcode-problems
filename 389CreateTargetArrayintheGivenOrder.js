802845f1250d9c2fe83e6a030a2172a7b8156523
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-07 15:03:53 -0800

    solved 389

---
+++ 389CreateTargetArrayintheGivenOrder.js
@@ -0,0 +1,7 @@
+var createTargetArray = function(nums, index) {
+    const result= [];
+    for(let i=0; i<index.length; i++) {
+      result.splice(index[i],0,nums[i])
+     }
+     return result
+  };
