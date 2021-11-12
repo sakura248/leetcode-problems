d9bdfd41a8418eec148035c21a333e3eb52c74f8
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-12 15:30:39 +0900

    today's solution

--- 1528-Shuffle-String.js
+++ 1528-Shuffle-String.js
@@ -1,2 +1,8 @@
-
-// ...
+var restoreString = function(s, indices) {
+    const tmp = new Array(indices.length)
+    for(let i=0; i<indices.length; i++){
+        tmp[indices[i]] = s[i]
+    }
+    return tmp.join('')
+    
+};
