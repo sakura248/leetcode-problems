f54cc37ced021b12bf784085784bb1da75e45f2a
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-25 16:52:23 -0800

    today's solution

---
+++ 704.js
@@ -0,0 +1,7 @@
+var search = function(nums, target) {
+    return nums.indexOf(target)
+
+};
+
+// Runtime: 84 ms, faster than 45.47% of JavaScript online submissions for Binary Search.
+// Memory Usage: 42.5 MB, less than 32.98% of JavaScript online submissions for Binary Search.
