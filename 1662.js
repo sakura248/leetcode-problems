f4571638b6e7d5142d4f8436629f8eed05368dda
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-27 18:00:51 -0800

    today's solution

---
+++ 1662.js
@@ -0,0 +1,7 @@
+
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join('') === word2.join('')
+};
+
+// Runtime: 99 ms, faster than 21.24% of JavaScript online submissions for Check If Two String Arrays are Equivalent.
+// Memory Usage: 38.9 MB, less than 78.94% of JavaScript online submissions for Check If Two String Arrays are Equivalent.
