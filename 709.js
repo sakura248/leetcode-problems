9597bd6de7b5279e3df2b87f484d7c00d83f7e15
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-29 15:12:36 -0800

    today's solution

---
+++ 709.js
@@ -0,0 +1,7 @@
+
+var toLowerCase = function(s) {
+    return s.toLowerCase()
+};
+
+// Runtime: 72 ms, faster than 76.46% of JavaScript online submissions for To Lower Case.
+// Memory Usage: 39.1 MB, less than 25.06% of JavaScript online submissions for To Lower Case.
